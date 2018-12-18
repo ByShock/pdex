0c643b395835704c3bddcc996982bd78734a953b
ByShock <Ivan.Lepkin@yandex.ru>
2018-12-18 21:58:27 +0300

    fullCard Component replaced with func component

--- src/components/fullCard.jsx
+++ src/components/fullCard.jsx
@@ -1,18 +1,21 @@
-import React, { Component } from 'react'
+import React, { useState, useEffect } from 'react'
 
 import '../styles/fullCard.css'
 
-export default class FullCard extends Component {
-  constructor (props) {
-    super(props)
-    this.state = {
-      imageUrls: [],
-      activeImage: undefined
+export default function FullCard (props) {
+  let [imageUrls, setImageUrls] = useState([])
+  let [activeImage, setActiveImage] = useState(null)
+  const handleClick = (e) => {
+    const images = document.getElementsByClassName('FullCard-smallImg')
+    for (let i = 0; i < images.length; i++) {
+      images[i].setAttribute('class', 'FullCard-smallImg')
     }
+    setActiveImage(e.target.attributes.src.value)
+
+    e.target.setAttribute('class', 'FullCard-smallImg FullCard-activeImage')
   }
-  componentDidMount () {
-    console.log(this.props)
-    const sprites = this.props.sprites
+  useEffect(() => {
+    const sprites = props.sprites
     const imageUrls = []
     for (const key in sprites) {
       if (sprites[key]) {
@@ -20,118 +23,95 @@ export default class FullCard extends Component {
         imageUrls.push(imgUrl)
       }
     }
-    this.setState({
-      imageUrls
-    })
-  }
-
-  handleClick = e => {
-    const images = document.getElementsByClassName('fullCard_small_img')
-    for (let i = 0; i < images.length; i++) {
-      images[i].setAttribute('class', 'fullCard_small_img')
-    }
-    this.setState({
-      activeImage: e.target.attributes.src.value
-    })
-
-    e.target.setAttribute('class', 'fullCard_small_img fullCard_activeImage')
-    console.log(this.state)
-  }
-  render () {
-    return (
-      <div className='fullCard_container'>
-        <h1 className='fullCard_header'>
-          {this.props.name[0].toUpperCase() + this.props.name.slice(1)}
-        </h1>
-        <div className='fullCard_content'>
-          <div className={'fullCard_imageBlock'}>
-            <img
-              className='fullCard_big_image'
-              src={this.state.activeImage || this.props.sprites.front_default}
-            />
-            <div>
-              {this.state.imageUrls.map((item, id) => {
-                return (
-                  <img
-                    className='fullCard_small_img'
-                    src={item}
-                    key={id}
-                    alt={'pokemon'}
-                    onClick={this.handleClick}
-                  />
-                )
-              })}
-            </div>
-          </div>
-          <div className='fullCard_info'>
-            <table className='table'>
-              <caption>Main Information</caption>
-              <thead>
-                <tr>
-                  <th>Stat</th>
-                  <th>Value</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <td>ID:</td>
-                  <td>{this.props.id}</td>
-                </tr>
-                <tr>
-                  <td>Height:</td>
-                  <td>{this.props.height}</td>
-                </tr>
-                <tr>
-                  <td>Weight:</td>
-                  <td>{this.props.weight}</td>
-                </tr>
-              </tbody>
-            </table>
-          </div>
+    setImageUrls(imageUrls)
+  })
+  return (
+    <div className='FullCard'>
+      <h1 className='FullCard-header'>
+        {props.name[0].toUpperCase() + props.name.slice(1)}
+      </h1>
+      <div className='FullCard-content'>
+        <div className={'FullCard-imageBlock'}>
+          <img
+            className='FullCard-bigImage'
+            src={activeImage || props.sprites.front_default}
+          />
           <div>
-            <table className='table'>
-              <caption>Main Stats</caption>
-              <thead>
-                <tr>
-                  <th>Stat</th>
-                  <th>Value</th>
-                </tr>
-              </thead>
-              <tbody>
-                {this.props.stats.map((item, id) => {
-                  if (!item) {
-                    return (
-                      <tr>
-                        <td>Something went wrong...</td>
-                      </tr>
-                    )
-                  }
+            {imageUrls.map((item, id) => {
+              return (
+                <img
+                  className='FullCard-smallImg'
+                  src={item}
+                  key={id}
+                  alt={'pokemon'}
+                  onClick={handleClick}
+                />
+              )
+            })}
+          </div>
+        </div>
+        <div className='fullCard_info'>
+          <table className='table'>
+            <caption>Main Information</caption>
+            <thead>
+              <tr>
+                <th>Stat</th>
+                <th>Value</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>ID:</td>
+                <td>{props.id}</td>
+              </tr>
+              <tr>
+                <td>Height:</td>
+                <td>{props.height}</td>
+              </tr>
+              <tr>
+                <td>Weight:</td>
+                <td>{props.weight}</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+        <div>
+          <table className='table'>
+            <caption>Main Stats</caption>
+            <thead>
+              <tr>
+                <th>Stat</th>
+                <th>Value</th>
+              </tr>
+            </thead>
+            <tbody>
+              {props.stats.map((item, id) => {
+                if (!item) {
                   return (
-                    /*
-                      <tr key={id}>
-                        <td>{item.stat.name}</td>
-                        <td>{item.base_stat}</td>
-                      </tr>
-                      */
-                    <tr key={id}>
-                      <td>{item.stat.name}</td>
-                      <td>
-                        <progress value={item.base_stat} max='100' />
-                        <span className='fullCard_progressDescription'>
-                          {item.base_stat}
-                        </span>
-                      </td>
+                    <tr>
+                      <td>Something went wrong...</td>
                     </tr>
                   )
-                })}
-              </tbody>
-            </table>
-          </div>
-        </div>
-        <div className='fullCard_close_button' onClick={this.props.closeCard}>
-          X
+                }
+                return (
+                  <tr key={id}>
+                    <td>{item.stat.name}</td>
+                    <td>
+                      <progress value={item.base_stat} max='100' />
+                      <span className='fullCard_progressDescription'>
+                        {item.base_stat}
+                      </span>
+                    </td>
+                  </tr>
+                )
+              })}
+            </tbody>
+          </table>
         </div>
       </div>
-    )
-  }
+      <div className='FullCard-closeButton' onClick={props.closeCard}>
+        X
+      </div>
+    </div>
+  )
 }
